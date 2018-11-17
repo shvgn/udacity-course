1953af167d006ab13cb84a7e5b61dc141c0b5d68
Evgeny Shevchenko <evgenii.shevchenko@fulldive.com>
2018-11-17 21:42:42 +0300

    Complete task 2

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -6,17 +6,27 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
+const _categoryHeight = 100.0;
+const _categoryBorderRadius = _categoryHeight / 2;
+
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
 class Category extends StatelessWidget {
+  final String name;
+  final Color color;
+  final IconData icon;
+
   /// Creates a [Category].
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
-  // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  const Category({
+    @required this.name,
+    @required this.icon,
+    @required this.color,
+  });
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -27,7 +37,31 @@ class Category extends StatelessWidget {
   // Theme ancestor in the tree. Below, we obtain the display1 text theme.
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
-    // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    return Container(
+      height: _categoryHeight,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(_categoryBorderRadius),
+        onTap: () {
+          print('Inkwell was tapped');
+        },
+        splashColor: color,
+        highlightColor: color,
+        child: Padding(
+          padding: EdgeInsets.all(4.0),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(icon, size: 60.0),
+              ),
+              Text(
+                name,
+                style: TextStyle(fontSize: 24),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
